2acaf3461bd6c4c63427e7af309639ebe3dbb84b
Matthew Ouyang <wouy448@aucklanduni.ac.nz>
2021-02-12 14:26:27 +1300

    fixed redux extension bug

--- jotter-client/src/redux/store.js
+++ jotter-client/src/redux/store.js
@@ -24,11 +24,10 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // for linking to redux extension COMMENT OUT FOR PRODUCTION
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
-
-console.log(store.getState());
 
 export default store;
 
